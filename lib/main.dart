1b16c32b38e869bef3af344256a0e77873caa1e1
hidro0 <hidr0zez0@gmail.com-toan27102003>
2023-03-17 23:21:03 +0700

    Bai_giua_ki

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
-import 'page1.dart';
+import 'package:bai_giua_ki/widgets/home.dart';
 void main(){
-  runApp(page1());
+  runApp(myApp());
 }
+class myApp extends StatelessWidget{
+  myApp({Key? key}) : super(key:key);
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
 
